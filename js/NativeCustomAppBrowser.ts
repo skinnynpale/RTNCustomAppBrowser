44b397902ce17f9f4eb79e7087fdcb6d56c0ccd7
skinnynpale <skinnynpale@gmail.com>
2024-02-02 00:51:52 +0300

    up

--- js/NativeCustomAppBrowser.ts
+++ js/NativeCustomAppBrowser.ts
@@ -1,5 +1,7 @@
 import { TurboModule, TurboModuleRegistry } from "react-native";
 
+// cancel — Пользователь сам закрыть браузер
+// dismiss — Сработал программный close
 export type CloseType = "cancel" | "dismiss";
 
 export interface Spec extends TurboModule {
